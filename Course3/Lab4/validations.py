e127e3116865f92ccf8913c0a993e41457b13c56
SurfacePro <ecommerce.websales@hotmail.com>
2022-03-24 21:15:39 +0000

    test Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # username can't start with non alpha
     if not username[0].isalpha():
         return False
 
